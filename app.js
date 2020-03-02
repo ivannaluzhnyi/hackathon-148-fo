827fbaec75dad50cd2487795e8e19aaec3687258
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2020-03-03 00:15:58 +0100

    app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 let app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
 app.use(express.static(path.join(__dirname, 'build')));
 const port = process.env.PORT || '8080';
 app.set('port', port);
