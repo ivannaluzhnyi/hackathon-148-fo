75f158e5c80dbb3ecb48b488005df7332059ef63
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2020-03-03 12:15:28 +0100

    add stepper

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../components/Button';
 import config from '../utils/config';
+import Stepper from '../components/Stepper';
 
 const Home = () => {
+    const [activeStep, setActiveStep] = useState(1);
     return (
         <div>
             <h1>Home</h1>
@@ -10,6 +12,8 @@ const Home = () => {
             <Button>Test</Button>
 
             <p>test env : {config.URL}</p>
+
+            <Stepper activeStep={activeStep} />
         </div>
     );
 };
